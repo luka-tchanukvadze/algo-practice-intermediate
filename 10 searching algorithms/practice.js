d6f1375ac7c4460472a40dc05c4c6c1bc70612b9
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-06 19:24:34 +0400

    Binary Search Solution

--- 10 searching algorithms/practice.js
+++ 10 searching algorithms/practice.js
@@ -1,8 +1,17 @@
-function linearSearch(arr, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === value) return i;
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  let middle = Math.floor((start + end) / 2);
+  while (start <= end && arr[middle] !== val) {
+    if (arr[middle] > val) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
   }
-  return "could not find";
+  return val !== arr[middle] ? -1 : middle;
 }
 
-console.log(linearSearch([2, 3, 4, 5], 5));
+console.log(binarySearch([1, 2, 3], 3));
