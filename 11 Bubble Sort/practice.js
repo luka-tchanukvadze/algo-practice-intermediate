3a1725857ea1718688924af68be9b230b89b63f3
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-07 21:32:27 +0400

    bubble sort

--- 11 Bubble Sort/practice.js
+++ 11 Bubble Sort/practice.js
@@ -1,1 +1,17 @@
-// Built-In JavaScript Sorting
+function bubbleSort(arr) {
+  for (let i = arr.length; i < 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] === arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([37, 45, 29, 9]));
