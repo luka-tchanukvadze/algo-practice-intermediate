93d79d2d93aee03f298b110d3c80f64142c29966
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-08 23:12:53 +0400

    Bubble Sort Optimization

--- 11 Bubble Sort/practice.js
+++ 11 Bubble Sort/practice.js
@@ -1,14 +1,21 @@
 function bubbleSort(arr) {
-  for (let i = arr.length; i < 0; i--) {
+  let noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+
     for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+      // console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         // swap
         let temp = arr[j];
         arr[j] === arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+
+    if (noSwaps) break;
   }
 
   return arr;
