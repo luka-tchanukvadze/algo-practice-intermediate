875b4b35791e28e60740add423db85c021244e97
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-10 20:46:58 +0400

    Comparing Bubble, Selection, and Insertion Sort

--- 13 Insertion Sort/practice.js
+++ 13 Insertion Sort/practice.js
@@ -1,7 +1,6 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-
     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
