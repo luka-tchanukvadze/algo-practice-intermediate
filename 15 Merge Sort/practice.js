98708624b7f2604b1a20e129ce131f52d49da491
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-12 17:38:47 +0400

    merge sort

--- 15 Merge Sort/practice.js
+++ 15 Merge Sort/practice.js
@@ -19,4 +19,13 @@ function merge(arr1, arr2) {
   return results;
 }
 
-console.log(merge([3, 4, 5], [1, 2, 3]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+// console.log(merge([3, 4, 5], [1, 2, 3]));
+console.log(mergeSort([1, 3, 2, 5, 6, 8]));
