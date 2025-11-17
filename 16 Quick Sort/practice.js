c8861e5684047f8298c6ce83c89e5aafeedd554d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-17 18:07:36 +0400

    Pivot Helper Implementation

---
+++ 16 Quick Sort/practice.js
@@ -0,0 +1,20 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+    swap(arr, start, swapIdx);
+  }
+}
+
+console.log(pivot([4, 9, 2, 1, 5, 7, 6, 3]));
