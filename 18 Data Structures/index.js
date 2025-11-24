8fc8681f43ad6c39861c027bf1a2d74d03b4c9f1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-24 21:17:32 +0400

    little bit of practice

---
+++ 18 Data Structures/index.js
@@ -0,0 +1,11 @@
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.year = year;
+  }
+}
+
+let firstStudent = new Student("luka", "chanu", 10);
+let secondStudent = new Student("luka", "chanu", 10);
+console.log(firstStudent);
