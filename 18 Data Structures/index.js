331cf6fb669cc1c652c010f2ab4a9fb96f25e771
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-24 21:21:57 +0400

    Data Structures Adding Class Methods

--- 18 Data Structures/index.js
+++ 18 Data Structures/index.js
@@ -3,9 +3,30 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
     this.year = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName} / ${this.year} years old`;
+  }
+
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return `You are experlled!!!`;
+    }
+    return `${this.firstName} has been late ${this.tardies} times`;
+  }
+
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
   }
 }
 
 let firstStudent = new Student("luka", "chanu", 10);
 let secondStudent = new Student("luka", "chanu", 10);
 console.log(firstStudent);
+console.log(firstStudent.markLate());
+console.log(firstStudent.addScore(30));
