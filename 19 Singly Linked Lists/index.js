e37e9a6a18457501db7c45a34536338eb51915c6
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-25 18:57:11 +0400

    Linked List Pop

--- 19 Singly Linked Lists/index.js
+++ 19 Singly Linked Lists/index.js
@@ -27,6 +27,25 @@ class SinglyLinedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 // let first = new Node("hi");
