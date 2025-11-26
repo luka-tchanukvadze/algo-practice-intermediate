4c582a9869a045b246793161c177b95e2ac9e99e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-26 20:28:19 +0400

    LL Insert

--- 19 Singly Linked Lists/index.js
+++ 19 Singly Linked Lists/index.js
@@ -90,6 +90,22 @@ class SinglyLinedList {
     }
     return false;
   }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+
+    return true;
+  }
 }
 
 // let first = new Node("hi");
