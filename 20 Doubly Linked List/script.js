157318a5c45574cc020ccdb737b8263e66391a33
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-01 20:10:21 +0400

    DLL Get

--- 20 Doubly Linked List/script.js
+++ 20 Doubly Linked List/script.js
@@ -73,4 +73,29 @@ class DoublyLinedList {
 
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let count, current;
+    const middle = this.length / 2;
+
+    if (index <= middle) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+
+    return current;
+  }
 }
