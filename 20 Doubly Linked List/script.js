78b37f1a32f9f51e8efb9e96e79886b34b8375de
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-01 19:48:00 +0400

    DLL Shift

--- 20 Doubly Linked List/script.js
+++ 20 Doubly Linked List/script.js
@@ -42,4 +42,20 @@ class DoublyLinedList {
 
     return removedTail;
   }
+
+  shift() {
+    if (!this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+
+    this.length--;
+    return oldHead;
+  }
 }
