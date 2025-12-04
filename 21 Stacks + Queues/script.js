2602c793475a7157cf83b1603f308fb7857465b2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-04 17:55:22 +0400

    Writing Our Own Stack From Scratch

--- 21 Stacks + Queues/script.js
+++ 21 Stacks + Queues/script.js
@@ -1,7 +1,43 @@
-let stack = [];
-stack.push("google");
-stack.push("instagram");
-stack.pop();
+// let stack = [];
+// stack.push("google");
+// stack.push("instagram");
+// stack.pop();
 
-stack.unshift("Create a new file");
-stack.shift();
+// stack.unshift("Create a new file");
+// stack.shift();
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+}
+
+let stack = new Stack();
+
+stack.push("FIRST");
+stack.push("second");
+stack.push("FIRST");
+stack.push("FIRST");
