7c002accab2dc96d88fc5225bee4c012fc58b33d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-05 19:16:31 +0400

    Writing Queue From Scratch

--- 21 Stacks + Queues/script.js
+++ 21 Stacks + Queues/script.js
@@ -83,7 +83,18 @@ class Queue {
     this.size = 0;
   }
 
-  enqueue(val) {}
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.size;
+  }
 
   dequeue(val) {}
 }
