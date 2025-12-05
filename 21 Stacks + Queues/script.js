68821fafdf6cafb53102f9ab724ea67bb56433ab
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-05 18:35:25 +0400

    Queues

--- 21 Stacks + Queues/script.js
+++ 21 Stacks + Queues/script.js
@@ -1,57 +1,57 @@
-// let stack = [];
-// stack.push("google");
-// stack.push("instagram");
-// stack.pop();
+// // let stack = [];
+// // stack.push("google");
+// // stack.push("instagram");
+// // stack.pop();
 
-const { ReceiptPoundSterling } = require("lucide-react");
+// const { ReceiptPoundSterling } = require("lucide-react");
 
-// stack.unshift("Create a new file");
-// stack.shift();
+// // stack.unshift("Create a new file");
+// // stack.shift();
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+// class Stack {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
 
-  push(val) {
-    let newNode = new Node(val);
-    if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      let temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
-    }
+//   push(val) {
+//     let newNode = new Node(val);
+//     if (!this.first) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       let temp = this.first;
+//       this.first = newNode;
+//       this.first.next = temp;
+//     }
 
-    return ++this.size;
-  }
+//     return ++this.size;
+//   }
 
-  pop() {
-    if (!this.first) return null;
-    let temp = this.first;
-    if (this.first === this.last) {
-      this.last = null;
-    }
+//   pop() {
+//     if (!this.first) return null;
+//     let temp = this.first;
+//     if (this.first === this.last) {
+//       this.last = null;
+//     }
 
-    this.first = this.first.next;
-    this.size--;
-    return temp.value;
-  }
-}
+//     this.first = this.first.next;
+//     this.size--;
+//     return temp.value;
+//   }
+// }
 
-let stack = new Stack();
+// let stack = new Stack();
 
-stack.push("FIRST");
-stack.push("second");
-stack.push("FIRST");
-stack.push("FIRST");
+// stack.push("FIRST");
+// stack.push("second");
+// stack.push("FIRST");
+// stack.push("FIRST");
