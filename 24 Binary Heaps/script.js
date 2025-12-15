c0887f0ef32ab8b248a6697d120ef3c0b9cc53a8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-15 18:19:49 +0400

    Heap ExtractMax Solution

--- 24 Binary Heaps/script.js
+++ 24 Binary Heaps/script.js
@@ -22,6 +22,54 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChlid, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChlid = this.values[leftChildIdx];
+        if (leftChlid > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChlid)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
