4bd1e3499c5a9b593daec1037823d67af13cf566
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-17 19:14:36 +0400

    Hash Table Set and Get Solution

--- 25 Hash Tables/script.js
+++ 25 Hash Tables/script.js
@@ -13,3 +13,47 @@ function hash(key, arrayLen) {
 }
 
 hash("pink", 9);
+
+///////////////
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.lenght, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].lenght; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i];
+        }
+      }
+    }
+
+    return undefined;
+  }
+}
