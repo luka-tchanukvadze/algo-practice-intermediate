7bfe1fb892cc2ace6c2841be3f98de0a1fcdd8d8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-23 18:34:54 +0400

    DFS Recursive Solution

--- 27 Graph Traversal/script.js
+++ 27 Graph Traversal/script.js
@@ -1,1 +1,47 @@
-// Depth First Graph Traversal
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v1] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacenyVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacenyVertex);
+    }
+
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFirstRecurseve(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+}
