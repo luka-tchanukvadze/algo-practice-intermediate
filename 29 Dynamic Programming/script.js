94959487fba48f4d6cc703267ce277902b1133bc
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-01-22 20:53:16 +0400

    Enter Memoization!

--- 29 Dynamic Programming/script.js
+++ 29 Dynamic Programming/script.js
@@ -1,4 +1,7 @@
-function fib(n) {
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
   if (n <= 2) return 1;
-  return fib(n - 1) + fix(n - 2);
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
 }
