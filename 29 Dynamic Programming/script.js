bf637d6bbc972dcd1196c55c435352b9852c43fe
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-01-22 21:04:38 +0400

    Tabulation A Bottom Up Approach

--- 29 Dynamic Programming/script.js
+++ 29 Dynamic Programming/script.js
@@ -1,7 +1,18 @@
-function fib(n, memo = []) {
-  if (memo[n] !== undefined) return memo[n];
+// function fib(n, memo = []) {
+//   if (memo[n] !== undefined) return memo[n];
+//   if (n <= 2) return 1;
+//   let res = fib(n - 1, memo) + fib(n - 2, memo);
+//   memo[n] = res;
+//   return res;
+// }
+
+function fib(n) {
   if (n <= 2) return 1;
-  let res = fib(n - 1, memo) + fib(n - 2, memo);
-  memo[n] = res;
-  return res;
+  let fibNums = [0, 1, 1];
+
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+
+  return fibNums[n];
 }
